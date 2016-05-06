be76facf0d3a27835936d852cee61f10c226ab53
Ron <ronll64@gmail.com>
2016-05-06 18:47:44 +0300

    made some progress

--- ihighcharts/timeline.py
+++ ihighcharts/timeline.py
@@ -12,6 +12,69 @@ def display_chart(events):
     y['C']=np.random.choice(['red','blue'],size=10)
     y['B'] = y['B']+1
 
+    base_dict = {
+        'chart': {
+            'type': 'line',
+            },
+        'series': [],
+        'xAxis': {
+            'gridLineWidth': 1,
+            'type': 'datetime',
+            'plotBands': [{
+                u'color': u'#FCFFC5',
+                # u'from': 4.5,
+                u'id': u'plotband-1',
+                # u'to': 7.5
+                }]
+            }
+
+        }
+
+    x = pd.read_csv('/home/ron/notebooks/data.csv')
+    data = x[["date", "bank_status"]].to_dict('split')
+    data.pop('columns', None)
+    data.pop('index', None)
+
+    base_dict['series'].append(data)
 
 
-    pass
+
+
+    # base_dict = \
+    #   {u'series': [
+    #       {u'data':
+    #            [29.9,
+    #                 71.5,
+    #                 106.4,
+    #                 129.2,
+    #                 144,
+    #                 176,
+    #                 135.6,
+    #                 148.5,
+    #                 216.4,
+    #                 194.1,
+    #                 95.6,
+    #            54.4]}],
+    #            u'xAxis':
+    #            {u'categories':
+    #                 [u'Jan',
+    #                      u'Feb',
+    #                      u'Mar',
+    #                      u'Apr',
+    #                      u'May',
+    #                      u'Jun',
+    #                      u'Jul',
+    #                      u'Aug',
+    #                      u'Sep',
+    #                      u'Oct',
+    #                      u'Nov',
+    #                 u'Dec'],
+    #                 u'plotBands':
+    #                 [{u'color': u'#FCFFC5',
+    #                       u'from': 4.5,
+    #                       u'id': u'plotband-1',
+    #                       u'to': 7.5}]}}
+
+
+
+    # pass
